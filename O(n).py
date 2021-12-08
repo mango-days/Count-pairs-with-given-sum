0bf947862fedca3453fff4b7e9fc97300ec1f9cc
mango-days <77330707+mango-days@users.noreply.github.com>
2021-12-09 00:13:01 +0530

    Update O(n).py

--- O(n).py
+++ O(n).py
@@ -1,1 +1,17 @@
+array = [ 1, 5, 7, 1 ]
+req_sum = 6
 
+pair_count = 0
+library = {}
+
+for index in range ( 0 , len ( array ) ) :
+    temp = array [ index ]
+    if temp not in library : library [ temp ] = 1
+    else : library [ temp ] += 1
+    
+for x in library.keys() :
+    temp = req_sum - x
+    if temp in library.keys() : 
+        pair_count += 1 
+        
+print ( pair_count ) 
